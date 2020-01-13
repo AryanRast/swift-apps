05281e9e1ad181c83a9b3828df832a3a9b4ee5ed
AryanRast <rastogi.a@etoncollege.org.uk>
2020-01-13 21:24:31 +0000

    EW2

---
+++ CarCustomizer/CarCustomizer/StarterCars.swift
@@ -0,0 +1,14 @@
+//
+//  StarterCars.swift
+//  CarCustomizer
+//
+//  Created by Rastogi, Aryan (WING) on 13/01/2020.
+//  Copyright © 2020 Rastogi, Aryan (WING). All rights reserved.
+//
+
+import Foundation
+
+struct StarterCars {
+    var cars = [Car(make: "Ferrari", model: "La-Ferrari", topSpeed: 250, acceleration: 1.2, handling: 9), Car(make: "Bugatti", model: "Chiron", topSpeed: 270, acceleration: 1, handling: 8), Car(make: "Lamborghini", model: "Aventador", topSpeed: 245, acceleration: 1.3, handling: 9), Car(make: "Nano", model: "Nano", topSpeed: 50, acceleration: 35, handling: 4), Car(make: "Land Rover", model: "Evoke", topSpeed: 180, acceleration: 3.5, handling: 9), Car(make: "Rolls-Royce", model: "Phantom", topSpeed: 160, acceleration: 5, handling: 10)
+    ]
+}
--- CarCustomizer/CarCustomizer/ViewController.swift
+++ CarCustomizer/CarCustomizer/ViewController.swift
@@ -11,14 +11,99 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var carStatistics: UILabel!
     
-    var car = Car(make: "Ferrari", model: "La-Ferrari", topSpeed: 250, acceleration: 1.2, handling: 9)
+    
+    @IBOutlet var enginePackage: UISwitch!
+    @IBOutlet var tiresPackage: UISwitch!
+    @IBOutlet var nitroPackage: UISwitch!
+    @IBOutlet var ultraPackage: UISwitch!
+    
+  
+    var remainingFunds = 2000 {
+        didSet {
+            ultraPackage.isEnabled = true
+            tiresPackage.isEnabled = true
+            enginePackage.isEnabled = true
+            nitroPackage.isEnabled = true
+            remainingFundsDisplay.text = "Remaining Funds: $\(remainingFunds)"
+            if remainingFunds < 500 {
+                tiresPackage.isEnabled = false
+                enginePackage.isEnabled = false
+            } else if remainingFunds < 750 {
+                nitroPackage.isEnabled = false
+            } else if remainingFunds < 2000 {
+                ultraPackage.isEnabled = false
+            }
+        }
+    }
+    var starterCars = StarterCars()
+    var index = 0
+    var car: Car? {
+        didSet {
+            carStatistics.text = car?.displayStats()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        carStatistics.text = car.displayStats()
+        car = starterCars.cars[index]
+        remainingFundsDisplay.text = "Remaining Funds: $\(remainingFunds)"
     }
-
+    
+    @IBAction func nextCar(_ sender: Any) {
+        index += 1
+        if index >= starterCars.cars.count {
+            index = 0
+        }
+        car = starterCars.cars[index]
+      }
+    
+    
+    @IBAction func engineToggle(_ sender: Any) {
+        if enginePackage.isOn {
+            car?.topSpeed += 50
+            remainingFunds -= 500
+        } else{
+            car?.topSpeed -= 50
+            remainingFunds += 500
+        }
+    }
+    
+    @IBOutlet var remainingFundsDisplay: UILabel!
+    
+    @IBAction func tiresToggle(_ sender: Any) {
+        if tiresPackage.isOn {
+            car?.handling += 1
+            remainingFunds -= 500
+        } else {
+            car?.handling -= 1
+            remainingFunds += 500
+        }
+    }
+    
+    @IBAction func nitroToggle(_ sender: Any) {
+        if nitroPackage.isOn {
+            car?.acceleration -= 0.5
+            remainingFunds -= 750
+        } else {
+            car?.acceleration += 0.5
+            remainingFunds += 750
+        }
+    }
+    
+    @IBAction func ultraToggle(_ sender: Any) {
+        if ultraPackage.isOn {
+            car?.acceleration -= 0.5
+            car?.topSpeed += 30
+            car?.handling += 1
+            remainingFunds -= 2000
+        } else {
+            car?.acceleration += 0.5
+            car?.topSpeed -= 30
+            car?.handling -= 1
+            remainingFunds += 2000
+        }
+    }
+    
 
 }
 
