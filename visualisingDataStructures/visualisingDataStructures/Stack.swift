8ed58fbb95029daec98b185a5ee1c3d260c1eb1d
AryanRast <rastogi.a@etoncollege.org.uk>
2020-01-28 11:19:13 +0000

    not really working

--- visualisingDataStructures/visualisingDataStructures/Stack.swift
+++ visualisingDataStructures/visualisingDataStructures/Stack.swift
@@ -11,7 +11,25 @@ import Foundation
 struct Stack {
     
     var stack = [String](repeating: "_", count: 10)
-    var top = - 1
+    var top = -1
+    
+    mutating func pop(){
+        if top != -1 {
+            self.top -= 1
+        }
+    }
+    
+    mutating func push(item: String) {
+        if top <= stack.count - 1 {
+            self.top += 1
+            stack[top] = item
+            
+        }
+        
+    }
+    
+    func peak()
+    
     
     
     
